41fa8ed083a6dd076bc34c5c27cd5df3cbd3c188
DanyloSukhorukov <2dnchik@gmail.com>
2025-05-08 19:57:06 +0300

    Update next.config.mjs

--- inventory-management-master/client/next.config.mjs
+++ inventory-management-master/client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "managment-hapis-s3-norepeat.s3.eu-west-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
